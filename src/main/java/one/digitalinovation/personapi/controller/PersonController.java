074d183b5f3fdecb667794782af13ae603772621
MauroAlbrecht <mauroint@gmail.com>
2021-08-17 23:06:48 -0300

    Refatoração na API de gravação de pessoa

--- src/main/java/one/digitalinovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinovation/personapi/controller/PersonController.java
@@ -1,8 +1,8 @@
 package one.digitalinovation.personapi.controller;
 
 import lombok.AllArgsConstructor;
+import one.digitalinovation.personapi.dto.request.PersonDTO;
 import one.digitalinovation.personapi.dto.response.MessageResponseDTO;
-import one.digitalinovation.personapi.entity.Person;
 import one.digitalinovation.personapi.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,7 +19,7 @@ public class PersonController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public MessageResponseDTO create(@RequestBody @Valid Person person) {
-        return personService.createPerson(person);
+    public MessageResponseDTO create(@RequestBody @Valid PersonDTO personDTO) {
+        return personService.createPerson(personDTO);
     }
 }
--- src/main/java/one/digitalinovation/personapi/services/PersonService.java
+++ src/main/java/one/digitalinovation/personapi/services/PersonService.java
@@ -1,5 +1,7 @@
 package one.digitalinovation.personapi.services;
 
+import one.digitalinovation.personapi.dto.mapper.PersonMapper;
+import one.digitalinovation.personapi.dto.request.PersonDTO;
 import one.digitalinovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinovation.personapi.entity.Person;
 import one.digitalinovation.personapi.repositories.PersonRepository;
@@ -11,13 +13,17 @@ public class PersonService {
 
     private PersonRepository personRepository;
 
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
     @Autowired
     public  PersonService(PersonRepository personRepository){
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson (Person person) {
-        Person savedPerson = personRepository.save(person);
+    public MessageResponseDTO createPerson (PersonDTO personDTO) {
+        Person personToSave = personMapper.toModel(personDTO);
+
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Created person with 10"+ savedPerson.getId())
